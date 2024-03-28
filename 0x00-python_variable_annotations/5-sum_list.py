d71e774118ef05813c587441ee5bc56b7c9643d1
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-03-28 20:35:03 +0000

    Added task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Returns the sum of a list as a float"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of a list as float"""
+
+    return sum(input_list)
